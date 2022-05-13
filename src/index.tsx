b4f4ed352b049748a5d50e3b31c7060e1851ecc8
ancyloce <ancyloce@gmail.com>
2022-05-13 18:42:38 +0800

    :art: feat: added history & routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
+import { LocaleProvider } from '@douyinfe/semi-ui';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import history from './history';
+import US from './locale/en_US';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    // semi design is buggy, turn off strict mode temporarily
+    // <React.StrictMode>
+    <HistoryRouter history={history}>
+        <LocaleProvider locale={US}>
+            <App />
+        </LocaleProvider>
+    </HistoryRouter>
+    // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
