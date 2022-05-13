d2677394bc83154546d117600bbb682a17d2fb44
ancyloce <ancyloce@gmail.com>
2022-05-13 18:40:22 +0800

    :globe_with_meridians: feat: added en_US & zh_CN

---
+++ src/locale/en_US.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line camelcase
+import en_US from '@douyinfe/semi-ui/lib/es/locale/source/en_US';
+
+export default {
+    'menu.dashboard': 'Dashboard',
+    'menu.list': 'List',
+    'menu.result': 'Result',
+    'menu.exception': 'Exception',
+    'menu.form': 'Form',
+    'menu.profile': 'Profile',
+    'menu.visualization': 'Data Visualization',
+    'menu.user': 'User Center',
+    'navbar.docs': 'Docs',
+    // eslint-disable-next-line camelcase
+    ...en_US,
+};
---
+++ src/locale/zh_CN.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line camelcase
+import zh_CN from '@douyinfe/semi-ui/lib/es/locale/source/zh_CN';
+
+export default {
+    'menu.dashboard': '仪表盘',
+    'menu.list': '列表页',
+    'menu.result': '结果页',
+    'menu.exception': '异常页',
+    'menu.form': '表单页',
+    'menu.profile': '详情页',
+    'menu.visualization': '数据可视化',
+    'menu.user': '个人中心',
+    'navbar.docs': '文档中心',
+    // eslint-disable-next-line camelcase
+    ...zh_CN,
+};
