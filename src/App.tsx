7f2657b6e1f85dddc6c6166b529247577953f40d
ancyloce <ancyloce@gmail.com>
2022-05-16 14:54:22 +0800

    :globe_with_meridians: feat: added locale Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { LocaleProvider } from '@douyinfe/semi-ui';
 import PageLayout from './layout/layout';
+import { GlobalContext } from './context';
 
 function App() {
+    const localeName = localStorage.getItem('lang') || 'en_US';
+    const [locale, setLocale] = useState();
+
+    async function fetchLocale(ln?: string) {
+        const i18n = (await import(`./locale/${ln}.ts`)).default;
+        setLocale(i18n);
+    }
+
+    useEffect(() => {
+        fetchLocale(localeName);
+    }, []);
+
     return (
-        <Routes>
-            <Route path="/" element={<PageLayout />} />
-        </Routes>
+        <LocaleProvider locale={locale}>
+            <GlobalContext.Provider value={{ locale }}>
+                <Routes>
+                    <Route path="/" element={<PageLayout />} />
+                </Routes>
+            </GlobalContext.Provider>
+        </LocaleProvider>
     );
 }
 
