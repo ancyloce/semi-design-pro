065ad5f64fab3ac427f25c90c280bdc34a33d2c0
ancyloce <ancyloce@gmail.com>
2022-05-16 14:54:22 +0800

    :globe_with_meridians: feat: added locale Context

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
-import { LocaleProvider } from '@douyinfe/semi-ui';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import history from './history';
-import US from './locale/en_US';
+import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
@@ -13,9 +11,7 @@ root.render(
     // semi design is buggy, turn off strict mode temporarily
     // <React.StrictMode>
     <HistoryRouter history={history}>
-        <LocaleProvider locale={US}>
-            <App />
-        </LocaleProvider>
+        <App />
     </HistoryRouter>
     // </React.StrictMode>
 );
