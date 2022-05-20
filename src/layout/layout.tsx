79fa800357fabac617828ced610c780111094410
ancyloce <ancyloce@gmail.com>
2022-05-20 11:52:28 +0800

    :art: feat: updated layout

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,105 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { Avatar, Breadcrumb, Button, Layout, Nav } from '@douyinfe/semi-ui';
-import { IconBell, IconBytedanceLogo, IconHelpCircle, IconSemiLogo } from '@douyinfe/semi-icons';
-import { Outlet, useLocation, useNavigate } from 'react-router';
-import useLocale from '../utils/useLocale';
-import { defaultRoute, routes } from '../routes';
+import React from 'react';
+import { Breadcrumb, Layout } from '@douyinfe/semi-ui';
+import { IconBytedanceLogo } from '@douyinfe/semi-icons';
+import { Outlet } from 'react-router';
+import Header from './header';
+import Menu from './menu';
 
 const PageLayout = () => {
-    const { Header, Footer, Sider, Content } = Layout;
-    const [selectedKeys, setSelectedKeys] = useState<string[]>([defaultRoute]);
-    const navigate = useNavigate();
-    const location = useLocation();
-    const locale = useLocale();
-
-    const getMenus: any = (data: any[]) => {
-        return data?.map((item) => {
-            return {
-                itemKey: item.key,
-                text: locale ? locale[item.name] || item.name : item.name,
-                icon: item.icon,
-                items: getMenus(item.children),
-            };
-        });
-    };
-
-    const onSelect = (key: number | string) => {
-        if (key) {
-            navigate(`${key}`, {});
-            setSelectedKeys([`${key}`]);
-        }
-    };
-
-    useEffect(() => {
-        const { pathname } = location;
-        setSelectedKeys([`${pathname}`.replace('/', '')]);
-    }, [location]);
+    const { Footer, Content } = Layout;
 
     return (
-        <Layout style={{ border: '1px solid var(--semi-color-border)' }}>
-            <Header style={{ backgroundColor: 'var(--semi-color-bg-1)' }}>
-                <div>
-                    <Nav mode="horizontal" defaultSelectedKeys={['Home']}>
-                        <Nav.Header>
-                            <IconSemiLogo style={{ width: '96px', height: '36px', fontSize: 36 }} />
-                        </Nav.Header>
-                        <span
-                            style={{
-                                color: 'var(--semi-color-text-2)',
-                            }}
-                        >
-                            <span
-                                style={{
-                                    marginRight: '24px',
-                                    color: 'var(--semi-color-text-0)',
-                                    fontWeight: '600',
-                                }}
-                            >
-                                模版推荐
-                            </span>
-                            <span style={{ marginRight: '24px' }}>所有模版</span>
-                            <span>我的模版</span>
-                        </span>
-                        <Nav.Footer>
-                            <Button
-                                theme="borderless"
-                                icon={<IconBell size="large" />}
-                                style={{
-                                    color: 'var(--semi-color-text-2)',
-                                    marginRight: '12px',
-                                }}
-                            />
-                            <Button
-                                theme="borderless"
-                                icon={<IconHelpCircle size="large" />}
-                                style={{
-                                    color: 'var(--semi-color-text-2)',
-                                    marginRight: '12px',
-                                }}
-                            />
-                            <Avatar color="orange" size="small">
-                                YJ
-                            </Avatar>
-                        </Nav.Footer>
-                    </Nav>
-                </div>
-            </Header>
+        <Layout>
+            <Header />
             <Layout>
-                <Sider style={{ backgroundColor: 'var(--semi-color-bg-1)' }}>
-                    <Nav
-                        style={{ maxWidth: 220, height: '100%' }}
-                        defaultSelectedKeys={selectedKeys}
-                        onSelect={({ itemKey, selectedKeys }) => {
-                            console.log(selectedKeys);
-                            onSelect(itemKey);
-                        }}
-                        limitIndent={false}
-                        items={getMenus(routes)}
-                        footer={{
-                            collapseButton: true,
-                        }}
-                    />
-                </Sider>
+                <Menu />
                 <Content
                     style={{
                         padding: '24px',
@@ -122,31 +35,31 @@ const PageLayout = () => {
                     >
                         <Outlet />
                     </div>
+                    <Footer
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            padding: '20px',
+                            color: 'var(--semi-color-text-2)',
+                            backgroundColor: 'rgba(var(--semi-grey-0), 1)',
+                        }}
+                    >
+                        <span
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <IconBytedanceLogo size="large" style={{ marginRight: '8px' }} />
+                            <span>Copyright © 2019 ByteDance. All Rights Reserved. </span>
+                        </span>
+                        <span>
+                            <span style={{ marginRight: '24px' }}>平台客服</span>
+                            <span>反馈建议</span>
+                        </span>
+                    </Footer>
                 </Content>
             </Layout>
-            <Footer
-                style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    padding: '20px',
-                    color: 'var(--semi-color-text-2)',
-                    backgroundColor: 'rgba(var(--semi-grey-0), 1)',
-                }}
-            >
-                <span
-                    style={{
-                        display: 'flex',
-                        alignItems: 'center',
-                    }}
-                >
-                    <IconBytedanceLogo size="large" style={{ marginRight: '8px' }} />
-                    <span>Copyright © 2019 ByteDance. All Rights Reserved. </span>
-                </span>
-                <span>
-                    <span style={{ marginRight: '24px' }}>平台客服</span>
-                    <span>反馈建议</span>
-                </span>
-            </Footer>
         </Layout>
     );
 };
